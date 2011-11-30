2e38c7d18074eb000c560c50b805b3f25e248084
Kun Wang <ifreedom.cn@gmail.com>
2011-11-30 09:44:40 +0800

    Add .bashrc.local.

--- bash.sh
+++ bash.sh
@@ -1,8 +1,6 @@
 export LANG="zh_CN.GBK"
 export OUTPUT_CHARSET="GBK"
 
-export PATH="$HOME/bin:$PATH"
-
 for f in ~/dotbash/.profile.d/*.sh; do
 	if [ -f $f ]; then
 		. $f
@@ -32,4 +30,10 @@ if [ -z "$PKG_CONFIG_PATH" ]; then
   export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:/mingw/lib/pkgconfig
 fi
 
+export PATH="$HOME/bin:$PATH"
+
 alias G='git'
+
+[[ -f ~/.bashrc.local ]] && source ~/.bashrc.local
+
+echo Bash Init complete!
